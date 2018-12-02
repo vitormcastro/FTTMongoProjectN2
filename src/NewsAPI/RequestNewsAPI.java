c6d92ca924e08d53790c6b3552add1df9c6da7e9
vitormcastro <vitormcastro@outlook.com>
2018-12-02 14:03:54 -0200

    Classe Base para mandar a requisição

---
+++ src/NewsAPI/RequestNewsAPI.java
@@ -0,0 +1,45 @@
+package NewsAPI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+
+class RequestNewsAPI {
+
+	private String apiKey;
+	
+	public RequestNewsAPI() {
+		// TODO Auto-generated constructor stub
+		apiKey = "d3c316bbacc14785bb044d3993630980";		
+		
+	}
+	
+	public String GetNews(String params) throws IOException {
+		
+		String resp = "";
+        
+		URL newsAPI = new URL("https://newsapi.org/v2/" + URLEncoder.encode(params, "UTF-8")+"&apiKey="+apiKey);
+
+		URLConnection newsConnection = newsAPI.openConnection();
+	
+		newsConnection.setConnectTimeout(5000); 
+		newsConnection.setReadTimeout(5000);
+     	      
+		newsConnection.setRequestProperty("Content-Type", "application/json");
+     	 
+		BufferedReader in = new BufferedReader(new InputStreamReader(newsConnection.getInputStream()));
+		      
+		resp = in.readLine();
+		        
+		in.close();
+		        
+		return resp;
+	}
+
+}
