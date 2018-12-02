db871093283e21a6f27e5e7d53d32f8dcf83e514
vitormcastro <vitormcastro@outlook.com>
2018-12-02 14:04:35 -0200

    Classe de busca Everything no NewsAPI

---
+++ src/NewsAPI/EverythingNewsAPI.java
@@ -0,0 +1,14 @@
+package NewsAPI;
+
+class EverythingNewsAPI {
+
+	private String uniqueKey;
+	
+	public EverythingNewsAPI() {
+		// TODO Auto-generated constructor stub
+		uniqueKey = "sources?";
+	}
+	
+	
+
+}
