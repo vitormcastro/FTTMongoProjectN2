b2e129860138f06d0192ecbfb4a151ab47b6b325
vitormcastro <vitormcastro@outlook.com>
2018-12-02 22:01:41 -0200

    Arruma o filtro e pede a Requisicao e manda a resposta

--- src/NewsAPI/SourcesNewsAPI.java
+++ src/NewsAPI/SourcesNewsAPI.java
@@ -1,14 +1,24 @@
 package NewsAPI;
 
-class EverythingNewsAPI {
+import java.io.IOException;
+
+import VOs.SourcesVO;
+
+public class SourcesNewsAPI {
 
 	private String uniqueKey;
 	
-	public EverythingNewsAPI() {
+	public SourcesNewsAPI() {
 		// TODO Auto-generated constructor stub
 		uniqueKey = "sources?";
 	}
 	
-	
+	public String GetNews(SourcesVO search) throws IOException {
+		String params = uniqueKey +"category="+search.getCategory()+"&language="+search.getLanguage()+"&country="+search.getCountry();
+		RequestNewsAPI request = new RequestNewsAPI();
+		return request.GetNews(params);
+		
+		
+	}
 
 }
